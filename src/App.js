06bbdd0b7d7927fdef1edbde4827d9961b29ede4
rijtim1 <regiontimsina@gmail.com>
2021-11-23 13:30:10 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './css/App.css'
+import Title from './components/Title'
 
-function App() {
+function App () {
+  const name = {
+    title: 'Mars Rover',
+    subtitle: 'Explore the surface of Mars'
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title name={name} />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Title extends React.Component {
+  render () {
+    return (
+        <div className="title">
+            <h1>NASA Mars Rover Photos API</h1>
+            <h2>Hello World</h2>
+        </div>
+    )
+  }
+}
