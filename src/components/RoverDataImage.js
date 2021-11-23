1c381ab7d3970255987c143a9be1a804bfc08509
rijtim1 <regiontimsina@gmail.com>
2021-11-23 18:44:35 -0500

    kisses

---
+++ src/components/RoverDataImage.js
@@ -0,0 +1,65 @@
+import React from 'react'
+// import Carousel from 'react-bootstrap/Carousel'
+// import CarouselItem from 'react-bootstrap/esm/CarouselItem'
+// import CarouselCaption from 'react-bootstrap/esm/CarouselCaption'
+import '../css/w3.css'
+
+export default class RoverDataImage extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            error: null,
+            isLoaded: false,
+            photos: []
+        }
+    }
+
+    componentDidMount () {
+        const BASE_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/'
+        const API_KEY = 'XAWxBohE3aseeg93CbssMc8e7ckcLD7VvxOKgUBY'
+        const url = BASE_URL + this.props.roverName + '/photos?api_key=' + API_KEY + '&sol=' + this.props.solValue
+        console.log(url)
+        fetch(url)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.photos
+                    })
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                }
+            )
+    }
+
+    render () {
+        // const { error, isLoaded, photos } = this.state
+        const { error, isLoaded } = this.state
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div className="w3-mobile">
+                    <p>The FUCKING IMAGE WONT LOAD AND I DONT UNDERSTAND WHY HELP!!!!!! IM LOSING BRAIN CELLS</p>
+                    {/* {
+                        photos.map(photo => (
+                            <div className="w3-container w3-mobile w3-animate-opacity">
+                                <img src={photo.img_src} alt={photo.earth_date} />
+                                
+                            </div>
+                        ))
+                    } */}
+                </div>
+                
+            )
+                        
+        }
+    }
+}
