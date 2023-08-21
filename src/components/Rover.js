8ccac3cef54bab68e296f93f2653c1628fe2815c
Rijan Timsina <regiontimsina@gmail.com>
2023-08-21 09:37:50 -0400

    refactor

--- src/components/Rover.js
+++ src/components/Rover.js
@@ -1,53 +1,55 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Container from 'react-bootstrap/Container'
-import RoverData from './RoverData'
-import Button from 'react-bootstrap/Button'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import RoverData from './RoverData';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.css';
 
-export default class Rover extends React.Component {
-  constructor (props) {
-    super(props)
+export default class Rover extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       selectRover: '',
       data: []
-    }
+    };
   }
 
-  onClick (roverName) {
+  onClick = (roverName) => {
     this.setState({
       selectRover: roverName.toLowerCase()
-    })
+    });
+  };
+
+  renderRoverButtons() {
+    return (
+      <div>
+        <h3 className="w3-margin">Select a Rover</h3>
+        {this.props.rovers.map((rover, index) => (
+          <Button
+            key={index}
+            variant="primary"
+            className="w3-margin"
+            onClick={() => this.onClick(rover)}
+          >
+            {rover}
+          </Button>
+        ))}
+      </div>
+    );
   }
 
-  render () {
+  render() {
+    const { selectRover } = this.state;
     return (
-            <Container className="">
-                <div className="w3-center w3-animate-bottom w3-mobile">
-                    {this.state.selectRover === '' ? 
-                    <div>
-                        <h3 className="w3-margin">Select a Rover </h3>
-                        {
-                                this.props.rovers.map((rover, index) => 
-                                {
-                                    return (
-                                        <Button key={index} variant="primary" className="w3-margin" onClick={() => this.onClick(rover)}>{rover}</Button>
-                                    )
-                                }
-                            )
-                        }
-                    </div>
-                    : 
-                    <div>
-                        <RoverData selectRover={this.state.selectRover}/>
-                    </div>
-                    }
-                </div>
-            </Container>
-    )
+      <Container className="">
+        <div className="w3-center w3-animate-bottom w3-mobile">
+          {selectRover === '' ? this.renderRoverButtons() : <RoverData selectRover={selectRover} />}
+        </div>
+      </Container>
+    );
   }
 }
 
 Rover.propTypes = {
   rovers: PropTypes.array
-}
+};
